1d15921372e10e9f636ab7263875b035febd2c8e
sk george <skgeorge@hotmail.com>
2019-09-18 11:21:26 +0530

    updated files from stephens repo

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t  stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker build -t skgeorge/multi-client:latest -t skgeorge/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t skgeorge/multi-server:latest -t skgeorge/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t skgeorge/multi-worker:latest -t skgeorge/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push skgeorge/multi-client:latest
+docker push skgeorge/multi-server:latest
+docker push skgeorge/multi-worker:latest
+
+docker push skgeorge/multi-client:$SHA
+docker push skgeorge/multi-server:$SHA
+docker push skgeorge/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=skgeorge/multi-server:$SHA
